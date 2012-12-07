a6b9dcb18478b83fcf17f361e4c81d2bf0c4fbf5
Dmytro V. Dogadailo <entropyhacker@gmail.com>
2012-12-07 20:11:36 +0200

    Refactored pack and unpack.

--- rjson.js
+++ rjson.js
@@ -95,58 +95,61 @@ var RJSON = (function() {
     function pack(data) {
         var schemas = {}, maxSchemaIndex = 0;
 
-        function encode(value) {
-            var encoded, i, j, k, v, current, last, len,
-            schema, schemaKeys, schemaIndex;
-            if (typeof value !== 'object' || !value) {
-                // non-objects or null return as is
-                return value;
+        function encodeArray(value) {
+            var len = value.length, encoded = [];
+            if (len === 0) return [];
+            if (typeof value[0] === 'number') {
+                encoded.push(0);  // 0 is schema index for Array
             }
-
-            if (isArray(value)) {
-                len = value.length;
-                if (len === 0) {
-                    return [];
-                }
-                encoded = [];
-                if (typeof value[0] === 'number') {
-                    encoded.push(0);  // 0 is schema index for Array
-                }
-                for (i = 0; i < len; i++) {
-                    v = value[i];
-                    current = encode(v);
-                    last = encoded[encoded.length - 1];
-                    if (isEncodedObject(current) &&
-                        isArray(last) && current[0] === last[0]) {
-                        // current and previous object have same schema,
-                        // so merge their values into one array
-                        encoded[encoded.length - 1] =
-                            last.concat(current.slice(1));
-                    } else {
-                        encoded.push(current);
-                    }
-                }
-            } else {
-                schemaKeys = getKeys(value).sort();
-                if (schemaKeys.length === 0) {
-                    return {};
-                }
-                schema = schemaKeys.length + ':' + schemaKeys.join('|');
-                schemaIndex = schemas[schema];
-                if (schemaIndex) { // known schema
-                    encoded = [schemaIndex];
-                    for (i = 0, k; k = schemaKeys[i++]; ) {
-                        encoded[i] = encode(value[k]);
-                   }
-                } else {    // new schema
-                    schemas[schema] = ++maxSchemaIndex;
-                    encoded = {};
-                    for (i = 0, k; k = schemaKeys[i++]; ) {
-                        encoded[k] = encode(value[k]);
-                    }
+            for (var i = 0; i < len; i++) {
+                var v = value[i],
+                current = encode(v),
+                last = encoded[encoded.length - 1];
+                if (isEncodedObject(current) &&
+                    isArray(last) && current[0] === last[0]) {
+                    // current and previous object have same schema,
+                    // so merge their values into one array
+                    encoded[encoded.length - 1] =
+                        last.concat(current.slice(1));
+                } else {
+                    encoded.push(current);
                 }
             }
             return encoded;
+        }
+
+        function encodeObject(value) {
+            var schemaKeys = getKeys(value).sort();
+            if (schemaKeys.length === 0) {
+                return {};
+            }
+            var encoded,
+            schema = schemaKeys.length + ':' + schemaKeys.join('|'),
+            schemaIndex = schemas[schema];
+            if (schemaIndex) { // known schema
+                encoded = [schemaIndex];
+                for (var i = 0, k; k = schemaKeys[i++]; ) {
+                    encoded[i] = encode(value[k]);
+                }
+            } else {    // new schema
+                schemas[schema] = ++maxSchemaIndex;
+                encoded = {};
+                for (var i = 0, k; k = schemaKeys[i++]; ) {
+                    encoded[k] = encode(value[k]);
+                }
+            }
+            return encoded;
+        }
+
+        function encode(value) {
+            if (typeof value !== 'object' || !value) {
+                // non-objects or null return as is
+                return value;
+            } else if (isArray(value)) {
+                return encodeArray(value);
+            } else {
+                return encodeObject(value);
+            }
         }
 
         return encode(data);
@@ -159,62 +162,75 @@ var RJSON = (function() {
     function unpack(data) {
         var schemas = {}, maxSchemaIndex = 0;
 
-        function decode(value) {
-            var decoded, i, j, k, v, obj, schemaKeys, schemaLen, total, len;
-            if (typeof value !== 'object' || !value) {
-                // non-objects or null return as is
-                return value;
+        function parseArray(value) {
+            if (value.length === 0) {
+                return [];
+            } else if (value[0] === 0 || typeof value[0] !== 'number') {
+                return decodeArray(value);
+            } else {
+                return decodeObject(value);
             }
+        }
 
-            if (isArray(value)) {
-                len = value.length;
-                if (len === 0) {
-                    decoded = [];
-                } else if (value[0] === 0 || typeof value[0] !== 'number') {
-                    decoded = []; // decode array of something
-                    for (i = (value[0] === 0 ? 1 : 0); i < len; i++) {
-                        v = value[i];
-                        obj = decode(v);
-                        if (isEncodedObject(v) && isArray(obj)) {
-                            // several objects was encoded into single array
-                            decoded = decoded.concat(obj);
-                        } else {
-                            decoded.push(obj);
-                        }
-                    }
+        function decodeArray(value) {
+            var len = value.length, decoded = []; // decode array of something
+            for (var i = (value[0] === 0 ? 1 : 0); i < len; i++) {
+                var v = value[i], obj = decode(v);
+                if (isEncodedObject(v) && isArray(obj)) {
+                    // several objects was encoded into single array
+                    decoded = decoded.concat(obj);
                 } else {
-                    schemaKeys = schemas[value[0]];
-                    schemaLen = schemaKeys.length;
-                    total = (value.length - 1) / schemaLen;
-                    if (total > 1) {
-                        decoded = []; // array of objects with same schema
-                        for (i = 0; i < total; i++) {
-                            obj = {};
-                            for (j = 0; k = schemaKeys[j++]; ) {
-                                obj[k] = decode(value[i * schemaLen + j]);
-                            }
-                            decoded.push(obj);
-                        }
-                    } else {
-                        decoded = {};
-                        for (j = 0, k; k = schemaKeys[j++]; ) {
-                            decoded[k] = decode(value[j]);
-                        }
-                    }
-                }
-
-            } else { // new schema
-                schemaKeys = getKeys(value).sort();
-                if (schemaKeys.length === 0) {
-                    return {};
-                }
-                schemas[++maxSchemaIndex] = schemaKeys;
-                decoded = {};
-                for (i = 0, k; k = schemaKeys[i++]; ) {
-                    decoded[k] = decode(value[k]);
+                    decoded.push(obj);
                 }
             }
             return decoded;
+        }
+
+        function decodeObject(value) {
+            var schemaKeys = schemas[value[0]],
+            schemaLen = schemaKeys.length,
+            total = (value.length - 1) / schemaLen,
+            decoded;
+            if (total > 1) {
+                decoded = []; // array of objects with same schema
+                for (var i = 0; i < total; i++) {
+                    var obj = {};
+                    for (var j = 0, k; k = schemaKeys[j++]; ) {
+                        obj[k] = decode(value[i * schemaLen + j]);
+                    }
+                    decoded.push(obj);
+                }
+            } else {
+                decoded = {};
+                for (var j = 0, k; k = schemaKeys[j++]; ) {
+                    decoded[k] = decode(value[j]);
+                }
+            }
+            return decoded;
+        }
+
+        function decodeNewObject(value) {
+            var schemaKeys = getKeys(value).sort();
+            if (schemaKeys.length === 0) {
+                return {};
+            }
+            schemas[++maxSchemaIndex] = schemaKeys;
+            var decoded = {};
+            for (var i = 0, k; k = schemaKeys[i++]; ) {
+                decoded[k] = decode(value[k]);
+            }
+            return decoded;
+        }
+
+        function decode(value) {
+            if (typeof value !== 'object' || !value) {
+                // non-objects or null return as is
+                return value;
+            } else if (isArray(value)) {
+                return parseArray(value);
+            } else { // object with new schema
+                return decodeNewObject(value);
+            }
         }
 
         return decode(data);
