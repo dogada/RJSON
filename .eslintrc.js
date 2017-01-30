7075cd59b3f0e39d027d7917e64263de315f0169
eugenioclrc <eugenioclrc@gmail.com>
2017-01-30 02:09:30 -0300

    cli es6

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,5 @@ module.exports = {
     "env": {
       "node": false,
       "browser": true,
-    },
-    /*"ecmaFeatures": {
-      "modules": false,
-    },*/
-    "rules": {
-      "no-console": [2, { allow: ["warn", "error", "err", "log"] }],
-      "new-cap": ["error", {"capIsNewExceptions": ["Q", "Q.Promise", "express.Router"]}]
     }
 };
